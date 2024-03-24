dfa96149323e2e0c91e6edbb70a02162dc9b6b97
Karan-Siddhpura <siddhpura306@gmail.com>
2024-03-24 19:46:51 +0530

    connection to mongo db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDb Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(3000, () => {
   console.log("Sever runing on port 3000");
